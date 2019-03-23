3cbd2ef8746a89bb8d8cf38f11c77efc4b73f182
AdirYed <adir.yed@gmail.com>
2019-03-23 12:57:43 +0200

    embed response

--- src/data/threads.js
+++ src/data/threads.js
@@ -11,8 +11,6 @@ const utils = require('../utils');
 const Thread = require('./Thread');
 const {THREAD_STATUS} = require('./constants');
 
-const discord = require('discord.js');
-
 /**
  * @param {String} id
  * @returns {Promise<Thread>}
