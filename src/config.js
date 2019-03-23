dac8935c421813493b7331ec626912872963b31e
AdirYed <adir.yed@gmail.com>
2019-03-23 12:18:30 +0200

    embed response

--- src/config.js
+++ src/config.js
@@ -53,15 +53,45 @@ class Moshe extends commando.Command {
   }
   
   async run(message, args) {
-  const responseEmbedMessage = new discord.RichEmbed()
-    .setColor('#008000')
-    .setTitle('Moshe')
-    .setDescription('Agever')
-    .addField('Adir', 'Yed', true)
-    .setTimestamp()
-    .setFooter('Admin'); 
+  // const responseEmbedMessage = new discord.RichEmbed()
+  //   .setColor('#008000')
+  //   .setTitle('Moshe')
+  //   .setDescription('Agever')
+  //   .addField('Adir', 'Yed', true)
+  //   .setTimestamp()
+  //   .setFooter('Admin'); 
   
-  message.channel.sendEmbed(responseEmbedMessage);
+  message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
+});
+
+  // message.channel.sendEmbed(responseEmbedMessage);
   }
 }
 
